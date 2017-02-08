1a2ad199d9d00f338940967dc3605fe8ab0656d1
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2017-02-08 14:28:37 +0100

    start UI

---
+++ EnsayosTraccionUAL/mainwindow.h
@@ -0,0 +1,39 @@
+/*+-------------------------------------------------------------------------+
+|                             LibreDAQ                                    |
+|                                                                         |
+| Copyright (C) 2015  Jose Luis Blanco Claraco                            |
+| Distributed under GNU General Public License version 3                  |
+|   See <http://www.gnu.org/licenses/>                                    |
++-------------------------------------------------------------------------+  */
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+//ArduinoDAQ_LowLevel.h
+
+#include <deque>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+private slots:
+	void on_actionRe_scan_devices_triggered();
+
+private:
+	Ui::MainWindow *ui;
+
+};
+
+#endif // MAINWINDOW_H
